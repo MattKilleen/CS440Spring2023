c757296a611d056329a440d529a296b13fd76a24
Matthew Killeen <58102276+MattKilleen@users.noreply.github.com>
2023-02-07 16:19:34 -0500

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,21 +1,22 @@
-from pyamaze import maze, agent
+import random
 
+def generate_random_maze(rows, cols):
+    maze = [[0 for j in range(cols)] for i in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
+            if i == 0 or i == rows-1 or j == 0 or j == cols-1:
+                maze[i][j] = 0
+            else:
+                maze[i][j] = random.choice([0, 1])
+    maze[1][1] = "a"
+    maze[rows-2][cols-2] = "g"
+    return maze
 
-def create_maze(width, height):
-    m = maze(width, height)
-    m.CreateMaze(loopPercent=50)
-    a = agent(m, filled=True, footprints=True)
-    return m, a
-
-
-def run_maze(m, a):
-    m.tracePath({a: m.path})
-    m.run()
-
-
-m, a = create_maze(101, 101)
-run_maze(m, a)
-
+rows = 5
+cols = 7
+maze = generate_random_maze(rows, cols)
+for row in maze:
+    print(row)
 
 def a_star(inital_position, goal_position, maze):
     pass
