56d61210b7638fe15939753ec8db18026c0ca981
Matthew Killeen <58102276+MattKilleen@users.noreply.github.com>
2023-02-07 15:37:37 -0500

    Add files via upload

---
+++ astar.py
@@ -0,0 +1,9 @@
+from pyamaze import maze,agent
+m=maze(20,20)
+m.CreateMaze(loopPercent=50)
+a=agent(m,filled=True,footprints=True)
+m.tracePath({a:m.path})
+m.run()
+
+def a_star(inital_position, goal_position, maze):
+    pass
