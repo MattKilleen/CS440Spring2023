58ad0ddcaac867a26c06890537b76b20993189f8
Matthew Killeen <58102276+MattKilleen@users.noreply.github.com>
2023-02-07 16:01:07 -0500

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,9 +1,21 @@
-from pyamaze import maze,agent
-m = maze(20,20)
-m.CreateMaze(loopPercent=50)
-a = agent(m,filled=True,footprints=True)
-m.tracePath({a:m.path})
-m.run()
+from pyamaze import maze, agent
+
+
+def create_maze(width, height):
+    m = maze(width, height)
+    m.CreateMaze(loopPercent=50)
+    a = agent(m, filled=True, footprints=True)
+    return m, a
+
+
+def run_maze(m, a):
+    m.tracePath({a: m.path})
+    m.run()
+
+
+m, a = create_maze(101, 101)
+run_maze(m, a)
+
 
 def a_star(inital_position, goal_position, maze):
     pass
