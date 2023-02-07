096fd6ab82d261095f21f027774d99009f1b4586
Matthew Killeen <58102276+MattKilleen@users.noreply.github.com>
2023-02-07 15:40:07 -0500

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,7 +1,7 @@
 from pyamaze import maze,agent
-m=maze(20,20)
+m = maze(20,20)
 m.CreateMaze(loopPercent=50)
-a=agent(m,filled=True,footprints=True)
+a = agent(m,filled=True,footprints=True)
 m.tracePath({a:m.path})
 m.run()
 
